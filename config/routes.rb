46f2b322fca91ee5c01f9fceafe5363a971fe4fd
EC2 Default User <ec2-user@ip-172-31-76-212.ec2.internal>
2022-06-09 08:39:29 +0000

    lists/delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
   get 'lists/new'
   post 'lists'=> 'lists#create'
-  get '/top' => 'homes#top'
+  get 'top' => 'homes#top'
+  get 'lists' => 'lists#index'
+  get 'lists/:id'=> 'lists#show', as: 'list'
+  get 'lists/:id/edit'=> 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
